58e953440b14e5a42b9b0ff8bb3ea3dfb7f49b86
antonin <anto94@live.ca>
2023-07-04 22:29:38 -0400

    Init

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let label: string;
+</script>
+
+<button
+	type="button"
+	class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
+	>{label}</button
+>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+	import Hero from '$lib/components/Hero.svelte';
+</script>
+
+<Header />
+<Hero />
+<slot />
