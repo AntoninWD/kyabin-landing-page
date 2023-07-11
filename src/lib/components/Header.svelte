9bec4bd880936bd5a241df7465775bc0f334cde6
antonin <anto94@live.ca>
2023-07-11 05:32:58 -0400

    Add brand logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,9 +4,8 @@
 <header>
 	<nav class="px-4 lg:px-6 py-2.5">
 		<div class="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl">
-			<a href="/" class="flex items-center">
-				<img src="/images/kyabin-logo.png" alt="Kyabin logo" class="w-12 h-12 mr-2" />
-				<span class="text-2xl font-semibold text-slate-900 font-cabin">Kyabin</span>
+			<a href="/" class="flex items-center w-48">
+				<img src="/images/kyabin-trans.png" alt="Kyabin" class="mr-2 h-full w-full" />
 			</a>
 		</div>
 	</nav>
